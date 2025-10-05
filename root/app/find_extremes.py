c415289b22402d219ffbd5a29d6ba0d0fa23674e
Afonso Vicente Nobre <a.m.vicentenobre@student.tudelft.nl>
2025-10-05 08:33:20 +0200

    More data

--- root/app/find_extremes.py
+++ root/app/find_extremes.py
@@ -10,10 +10,14 @@ import re
 from scipy.sparse import hstack, csr_matrix
 from textstat.textstat import textstat
 
-parent_dir = Path.cwd().parent
-database_path = parent_dir / "data" / "database" / "extremist_vs_appropriate_dataset.csv"
+database_dir = Path.cwd().parent / "data" / "database"
 
-df = pd.read_csv(database_path, usecols=["sentence", "label"])
+# Get all CSV files in the folder
+csv_files = list(database_dir.glob("*.csv"))
+
+dfs = [pd.read_csv(f, usecols=["sentence", "label"]) for f in csv_files]
+df = pd.concat(dfs, ignore_index=True)
+
 data = df["sentence"].astype(str).tolist()
 labels = df["label"].astype(int).tolist()
 
